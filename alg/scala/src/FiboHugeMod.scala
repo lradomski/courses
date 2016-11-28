0abe89f57c97d58c81a7283b5cbddaf84dab36bb
luke <luke>
2016-11-28 03:04:41 -0500

    Alg / week2 / fibo+gcd+lcm+fibo-huge-mod

--- alg/scala/src/FiboHugeMod.scala
+++ alg/scala/src/FiboHugeMod.scala
@@ -16,7 +16,7 @@ object FiboHugeMod {
 //    println( fiboHugeMod(99999999999999999L, 100*1000) )
   }
 
-  def fiboHugeMod(n: Long, mod: Int) : Int =
+  def fiboHugeMod2(n: Long, mod: Int) : Int =
   {
     require(n > 0, "n > 0")
     require(n <= 10e18, "n <= 10e18")
@@ -56,9 +56,13 @@ object FiboHugeMod {
     var i : Int = 2
     var period : Int = -1
 
+    def isPeriodStart(i : Int) : Boolean = (0 == fibo(i) && 1 == fibo(i+1))
+    def periodEnd(i : Int, period : Int) : Boolean = i == 3*period
+    def periodRepeats(i : Int, period : Int) : Boolean = fibo(i-period) == fibo(i)
+
     do
     {
-      while(i < n && (0 != fibo(i) || 1 != fibo(i+1)))
+      while(i < n && !isPeriodStart(i))
       {
 //          if (i % 500 == 0) println(i)
         i+=1
@@ -68,20 +72,87 @@ object FiboHugeMod {
       {
         period = i
         ensureArray(3*period)
-        println("Period: " + period + ", " + fibo(i) + ", " + fibo(i+1))
+        //println("Period: " + period + ", " + fibo(i) + ", " + fibo(i+1))
 
         i+=2
 
-        while (i < n && i < 3*period && fibo(i-period) == fibo(i))
+        while (i < n && !periodEnd(i, period) && periodRepeats(i, period))
         {
 //          if (i % 500 == 0) println(i)
           i+=1
 
         }
       }
-    } while (i != 3*period && i < n)
+    } while (!periodEnd(i, period) && i < n)
 
-    if (i == 3*period) fibo( (n % period).toInt ) else fiboMod(n, mod)
+    if (periodEnd(i, period)) fibo( (n % period).toInt ) else fiboMod(n, mod)
+  }
+
+  def fiboHugeMod(n: Long, mod: Int) : Int =
+  {
+    require(n > 0, "n > 0")
+    require(n <= 10e18, "n <= 10e18")
+    require(mod > 1, "mod > 1")
+    require(mod <= 10e5, "10e5")
+
+    var f : Array[Int] = new Array[Int](0)
+    var last : Int = 0
+
+    def ensureArray(n : Int) =
+    {
+      if (n >= f.length)
+      {
+        f = new Array[Int](n+1 + f.length/2)
+        f(0) = 0; f(1) = 1; f(2) = 1
+        last = 2
+      }
+
+    }
+
+    ensureArray(100*1000)
+
+    def fibo(n : Int) : Int =
+    {
+      ensureArray(n)
+
+      if (n > last) {
+        for (i <- last + 1 to n) f(i) = (f(i - 1) + f(i - 2)) % mod
+        last = n
+      }
+
+      f(n)
+    }
+
+    var i : Int = 2
+    var period : Int = -1
+
+    def isPeriodStart(i : Int) : Boolean = (0 == fibo(i) && 1 == fibo(i+1))
+    def periodEnd(i : Int, period : Int) : Boolean = i == 3*period
+    def periodRepeats(i : Int, period : Int) : Boolean = fibo(i-period) == fibo(i)
+
+    var findingPeriod = true
+    var periodFound = false
+
+    while (i < n  && !periodFound)
+    {
+      if (findingPeriod)
+      {
+        if (isPeriodStart(i))
+          {
+            period = i
+            ensureArray(i)
+            findingPeriod = false
+          }
+      }
+      else
+      {
+        if (periodRepeats(i, period)) periodFound = periodEnd(i, period) else findingPeriod = true
+      }
+
+      i+=1;
+    }
+
+    if (periodFound) fibo( (n % period).toInt ) else fiboMod(n, mod)
   }
 
   def fiboMod(n: Long, mod : Int): Int = {
