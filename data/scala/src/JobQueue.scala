c78fc61497901955b0ff161ce01f650a977fe7d4
luke <luke>
2016-12-03 19:18:31 -0500

    data / JobQueue

--- data/scala/src/JobQueue.scala
+++ data/scala/src/JobQueue.scala
@@ -3,9 +3,7 @@ import java.util.Scanner
 
 import scala.collection.mutable
 
-/**
-  * Created by luke on 12/3/16.
-  */
+
 object JobQueue
 {
 
@@ -28,49 +26,63 @@ object JobQueue
       tree
     }
 
-    //      for (i <- 1 to 24)
-    //        {
-    //          val name = "/Users/luke/git/courses/data/download/Starters PA1/tree_height/tests/%02d".format(i)
-    //          val s : Scanner = new Scanner( new File(name) )
-    //          val n = s.nextInt();
-    //          val jobs = readVertices(s, n)
-    //          val out = treeHeight(jobs)
-    //
-    //          val a = new Scanner( new File(name+".a")).nextInt();
-    //          val res = (a==out);
-    //          val line = "%02d".format(i)
-    //          println(s"${line}: ${res} (${out},${a})");
-    //        }
-
     val s : Scanner = if (args.isEmpty) new Scanner(System.in) else new Scanner( new File(args(0)) )
     val n = s.nextInt();
     val m = s.nextInt();
 
     val jobs = readLongs(s, m)
 
-    //val jobs : Array[Int] = List(5, 4, 3, 2, 1).toArray
+    //val jobs : Array[Long] = List(5, 4, 3, 2, 1).toArray
 
     val threads = jobQueue(n, jobs)
 
     assert(threads.length == jobs.length)
 
-    for (i <- 0 to jobs.length-1)
+    if (args.isEmpty)
     {
-      println(threads(i) + " " + jobs(i))
-
+      for (i <- 0 to jobs.length-1)
+      {
+        println(threads(i) + " " + jobs(i))
+      }
     }
+    else
+    {
+      val s2 = new Scanner( new File(args(0)+".a"))
+
+      var i = 0
+      while (i < jobs.length && s2.hasNext)
+        {
+          val t = s2.nextInt()
+          val s = s2.nextLong()
+          val ok = if (threads(i) == t && jobs(i) == s) "  " else "> "
+          println(ok + threads(i) + " " + jobs(i) + " / " + t + " " + s)
+          assert(threads(i) == t, "threads(i) == t")
+          assert(jobs(i) == s, "jobs(i) == s")
+          i += 1
+        }
+    }
+
 
 
   }
 
   def jobQueue(n : Int, jobs: Array[Long]) : Array[Int] =
   {
-    case class Pending(thread : Int, job : Int, start : Long)
+    case class Pending(thread : Int, job : Int, start : Long) extends Ordered[Pending]
+    {
+      override def compare(that: Pending): Int =
+      {
+        if (this.end == that.end) -1*this.thread.compare(that.thread)
+        else -1*this.end.compare(that.end)
+      }
+
+      def end = start+jobs(job)
+    }
 
 
     val threads = new Array[Int](jobs.length) // record of which thread processed which job
 
-    val jobsPending = new mutable.PriorityQueue[Pending]()(Ordering.by(p => -1*(p.start+jobs(p.job))))
+    val jobsPending = new mutable.PriorityQueue[Pending]() //(Ordering.by(p => -1*(p.start+jobs(p.job))))
 
     var time : Long = 0
 
@@ -78,7 +90,7 @@ object JobQueue
 
     while (i < n && i < jobs.length)
     {
-      jobsPending += (Pending(i, i, time))
+      jobsPending += Pending(i, i, time)
       i += 1
     }
 
