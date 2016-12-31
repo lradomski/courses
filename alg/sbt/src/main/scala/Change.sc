e6ea18b3faed68521b1d2b4bafd0e781e19430bb
luke <luke>
2016-12-30 22:14:35 -0500

    week3 - change+loot+ad/dotproduct+visit/segments+prizes(fast)+MaxSalary

--- alg/sbt/src/main/scala/Change.sc
+++ alg/sbt/src/main/scala/Change.sc
@@ -1,7 +1,12 @@
 import Change.change
+import MaxSalary.{makeNumber,lessThan}
 
-change(2)
-change(28)
+makeNumber(2,21)
+makeNumber(21,2)
+lessThan(2,21)
+lessThan(29,21)
+//change(2)
+//change(28)
 //change(194)
 
 
--- alg/sbt/src/main/scala/MaxSalary.scala
+++ alg/sbt/src/main/scala/MaxSalary.scala
@@ -2,14 +2,35 @@ import java.util.Scanner
 
 object MaxSalary
 {
+  def makeNumber(l: Int, r: Int): Int =
+  {
+    var tens = 10
+    while (r / tens > 0) tens *= 10
+    l * tens + r
+  }
+
+  def lessThan(l: Int, r: Int): Boolean =
+  {
+    assert(l <= 1e3.toInt)
+    assert(r <= 1e3.toInt)
+
+    makeNumber(l,r) < makeNumber(r, l)
+  }
+
+  def descending(l: Int, r: Int) = !lessThan(l,r)
+
   def main(args: Array[String]): Unit =
   {
+
     val s = new Scanner(System.in)
 
     val n = s.nextInt
 
-
-
+    val nums = new Array[Int](n)
+    for(i <- 0 to n-1) nums(i) = s.nextInt
+    val sorted = nums.sortWith(descending)
+    sorted.foreach(num => print(num))
+    println
   }
 
 }
