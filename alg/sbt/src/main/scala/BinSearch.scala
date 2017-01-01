0cb1f94ad34f33915fdbcd182c19f2574aeeed21
luke <luke>
2016-12-31 19:05:05 -0500

    week4 - stubs+BinSearch

--- alg/sbt/src/main/scala/BinSearch.scala
+++ alg/sbt/src/main/scala/BinSearch.scala
@@ -1,10 +1,28 @@
 import java.util.Scanner
 
+import scala.annotation.tailrec
+
 object BinSearch
 {
-  def find(array: Array[Int], i: Int): Int =
+  def find(a: Array[Int], key: Int): Int =
   {
-    -1
+    @tailrec
+    def core(start: Int, end: Int): Int =
+    {
+      if (end < start) start - 1
+      else
+      {
+        val mid = start + (end - start) / 2
+        val test = a(mid)
+        if (test == key) mid
+        else if (key < test) core(start, mid - 1)
+        else if (test > key) core(mid + 1, end)
+        else -1
+      }
+    }
+
+    val out = core(0, a.length - 1)
+    if (0 <= out && out < a.length && a(out) == key) out else -1
   }
 
   def main(args: Array[String]): Unit =
@@ -13,10 +31,10 @@ object BinSearch
 
     val n = s.nextInt
     val a = new Array[Int](n)
-    for (i <- 0 to n-1) a(i) = s.nextInt
+    for (i <- 0 to n - 1) a(i) = s.nextInt
 
     val k = s.nextInt
-    for (i <- 0 to k-1) print(find(a,s.nextInt) + " ")
+    for (i <- 0 to k - 1) print(find(a, s.nextInt) + " ")
     println
   }
 
