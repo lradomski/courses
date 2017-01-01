f073be1fe11176084f3a0d797cc348807dbfe098
luke <luke>
2017-01-01 02:17:02 -0500

    week4 - stubs+BinSearch+Majority+QuickSort3way

--- alg/sbt/src/test/scala/QuickSort3WayTest.scala
+++ alg/sbt/src/test/scala/QuickSort3WayTest.scala
@@ -7,7 +7,7 @@ extends FunSuite
   test("simple")
   {
     val a = List(5,4,3,2,1).toArray
-    qsort3way(a)
+    verify(qsort3way(a))
 
     verify(qsort3way(List(1).toArray))
     verify(qsort3way(List(2,1).toArray))
@@ -16,12 +16,17 @@ extends FunSuite
     verify(qsort3way(List(1,2).toArray))
     verify(qsort3way(List(1,2,3).toArray))
     verify(qsort3way(List(1,2,3,4,5).toArray))
-
   }
+
+  test("simple/2")
+  {
+    verify(qsort3way(List(60,118,118,166,60,209,289,118,209,747,821,289).toArray))
+  }
+
   test("randomized")
   {
     val r = scala.util.Random
-    for (c <- 1 to 100)
+    for (c <- 1 to 1000)
       {
         val a = new Array[Int](r.nextInt(100))
         for (i <- 0 to a.length-1) a(i) = r.nextInt(1000)
@@ -32,10 +37,12 @@ extends FunSuite
       }
   }
 
+  def toStr(a: Array[Int]): String = a.foldLeft("")((s,i) => s + i + ",")
+
 
   def verify(a: Array[Int]): Unit =
   {
     val as = a.sorted
-    for (i <- 0 to a.length - 1) assert(a(i) == as(i), "/ [" + i + "]: " + a(i) + "!=" + as(i))
+    for (i <- 0 to a.length - 1) assert(a(i) == as(i), "/ [" + i + "]: " + a(i) + "!=" + as(i) + " // {" + toStr(a) + "}")
   }
 }
