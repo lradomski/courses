01457509da051feb2a06bf5cf580cd6aea3d9666
luke <luke>
2017-01-14 01:48:54 -0500

    week5 - stubs+calc+TakeGold

--- alg/sbt/src/main/scala/TakeGold.scala
+++ alg/sbt/src/main/scala/TakeGold.scala
@@ -1,15 +1,27 @@
 import java.util.Scanner
 
+import scala.annotation.tailrec
+
 object TakeGold
 {
-  def main(args: Array[String]) =
+
+  @tailrec
+  def makeValues(max: Int, bars: List[Int], values: Set[Int]): Set[Int] = bars match
+  {
+    case Nil => values
+    case x :: xs => makeValues(max, xs, if (x<=max) values ++ values.map(_+x).filter(_<=max) else values)
+  }
+
+
+  def main(args: Array[String]): Unit =
   {
     val s = new Scanner(System.in)
 
-    val n = s.nextInt()
-    val capacity = s.nextInt()
+    val capacity = s.nextInt
+    val bars = for (i <- 1 to s.nextInt) yield s.nextInt
 
-    val bars = for (i <- 1 to n) yield s.nextInt
+    val taken = makeValues(capacity, bars.toList, Set(0)).max
+    println(taken)
 
   }
 }
