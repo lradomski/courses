5ac1bf8f9abaec29137eb740e710b95fc54e6e3a
luke <luke>
2017-01-14 23:26:11 -0500

    week5 - stubs+calc+TakeGold+EditDistance+MaxExpr

--- alg/sbt/src/main/scala/MaxExpr.scala
+++ alg/sbt/src/main/scala/MaxExpr.scala
@@ -3,13 +3,18 @@ import java.util.Scanner
 object MaxExpr
 {
 
-  case class MinMax(min: Long, max: Long, isSet: Boolean)
+//  case class MinMax(min: Long, max: Long, isSet: Boolean)
+  case class MinMax(min: Long, max: Long)
 
   val ops = Map(
     '+' -> ((l: Long, r: Long) => l + r),
     '-' -> ((l: Long, r: Long) => l - r),
     '*' -> ((l: Long, r: Long) => l * r)
   )
+
+//  val MinMaxInit = MinMax(Long.MaxValue, Long.MinValue)
+
+  def minMax(l: MinMax, r: MinMax) = MinMax(Math.min(l.min, r.min), Math.max(l.max,r.max))
 
   def calcOp(op: (Long, Long) => Long, l: MinMax, r: MinMax): MinMax =
   {
@@ -19,7 +24,7 @@ object MaxExpr
       rv <- List(r.min, r.max)
     } yield op(lv, rv)
 
-    MinMax(results.min, results.max, true)
+    MinMax(results.min, results.max)
   }
 
 
@@ -32,26 +37,28 @@ object MaxExpr
 
     def calcExp(i: Int, j: Int): MinMax =
     {
-      def subExp(i: Int, j: Int) =
-      {
-        assert(exp(i)(j).isSet)
-        exp(i)(j)
-      }
+//      def subExp(i: Int, j: Int) =
+//      {
+//        assert(exp(i)(j).isSet)
+//        exp(i)(j)
+//      }
 
-      assert(0 <= i && i < n.length, "is not: 0 <= " + i + " < " + n.length)
-      assert(i <= j && j < n.length, "is not: " + i + " <= " + j + " < " + n.length)
+//      assert(0 <= i && i < n.length, "is not: 0 <= " + i + " < " + n.length)
+//      assert(i <= j && j < n.length, "is not: " + i + " <= " + j + " < " + n.length)
 
-      if (i == j) MinMax(n(i), n(i), true)
-      else if (i + 1 == j) calcOp(o(i), subExp(i, i), subExp(j, j))
+      if (i == j) MinMax(n(i), n(i))
+//      else if (i + 1 == j) calcOp(o(i), subExp(i, i), subExp(j, j))
+      else if (i + 1 == j) calcOp(o(i), exp(i)(i), exp(j)(j))
       else
       {
-        val res = for (k <- i until j) yield calcOp(o(k), calcExp(i,k), calcExp(k+1,j))
-        MinMax(res.map(_.min).min, res.map(_.max).max, true)
+        val res = for (k <- i until j) yield calcOp(o(k), exp(i)(k), exp(k+1)(j))
+        res.foldLeft(MinMax(Long.MaxValue, Long.MinValue))(
+          (a,v) => MinMax(Math.min(a.min, v.min), Math.max(a.max,v.max))
+        )
+//        MinMax(res.map(_.min).min, res.map(_.max).max)
       }
 
     }
-
-    //for(i <- 0 until n.length) exp(i)(i) = MinMax(n(i), n(i))
 
     for
     {
@@ -60,7 +67,7 @@ object MaxExpr
       if (i + len < n.length)
     } exp(i)(i + len) = calcExp(i, i + len)
 
-    assert(exp(n.length-1)(n.length-1).isSet)
+//    assert(exp(n.length-1)(n.length-1).isSet)
     exp(0)(n.length-1).max
   }
 
