695b951459d98a75e4b49537183ceba7929c0e92
luke <luke>
2017-01-21 18:05:38 -0500

    week3 - Lottery

--- alg/sbt/src/main/scala/Lottery.scala
+++ alg/sbt/src/main/scala/Lottery.scala
@@ -2,14 +2,30 @@ import java.util.Scanner
 
 object Lottery
 {
-  case class Segment(left: Int, right: Int)
 
-  class Segments(segs: Array[Segment])
+  trait Point
   {
-    def countContaining(x: Int): Int =
-    {
-      0
-    }
+    def x: Int
+
+    def order: Int
+  }
+
+  case class Left(x: Int) extends Point
+  {
+    val order = 1
+    override def toString = "+" + x + " "
+  }
+
+  case class Right(x: Int) extends Point
+  {
+    val order = 3
+    override def toString = "-" + x + " "
+  }
+
+  case class Pick(x: Int, i: Int) extends Point
+  {
+    val order = 2
+    override def toString = "*" + x + " "
   }
 
   def main(args: Array[String]): Unit =
@@ -17,15 +33,54 @@ object Lottery
     val s = new Scanner(System.in)
 
     val segCount = s.nextInt
-    val pointCount = s.nextInt
+    val pickCount = s.nextInt
 
-    val segs = new Array[Segment](segCount)
-    val points = new Array[Int](pointCount)
+    var marks = new Array[Point](segCount * 2 + pickCount)
+    //val segs = new Array[Segment](segCount)
+    //val points = new Array[Int](pointCount)
+    var m = 0
 
-    for (i <- 0 to segCount-1) segs(i) = Segment(s.nextInt, s.nextInt)
+    for (i <- 0 until segCount)
+    {
+      marks(m) = Left(s.nextInt)
+      marks(m + 1) = Right(s.nextInt)
+      m += 2
+    }
 
-    val segsIndex = new Segments(segs)
-    for (i <- 0 to pointCount) print(segsIndex.countContaining(s.nextInt) + " ")
+    for (i <- 0 until pickCount)
+    {
+      marks(m) = Pick(s.nextInt, i)
+      m += 1
+    }
+
+
+
+    marks = marks.sortWith(
+      (l, r) =>
+      {
+        if (l.x < r.x) true
+        else if (l.x == r.x) l.order < r.order
+        else false
+      }
+    )
+
+
+
+    val picks = new Array[Int](pickCount)
+
+    var count = 0
+    for (i <- 0 until marks.length)
+    {
+      marks(i) match
+      {
+        case l: Left => count += 1
+        case r: Right => count -= 1
+        case p: Pick => picks(p.i) = count
+      }
+
+    }
+
+    picks.foreach(c => print(c + " "))
     println
   }
 
--- alg/sbt/src/main/scala/week5/week5.sc
+++ alg/sbt/src/main/scala/week5/week5.sc
@@ -1,4 +1,4 @@
 //val a = Array.ofDim[Boolean](2,2)
 //a(1)(1)
-//1 until 0
-"1+5".filterNot(_.isDigit)//.map(_.asDigit)
+1 until 3
+//"1+5".filterNot(_.isDigit)//.map(_.asDigit)
