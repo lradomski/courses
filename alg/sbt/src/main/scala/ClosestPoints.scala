94ebf3688d64abedb681912d62b071aad40fd580
luke <luke>
2017-01-22 16:14:56 -0500

    week3 - Lottery + ClosestPoints

--- alg/sbt/src/main/scala/ClosestPoints.scala
+++ alg/sbt/src/main/scala/ClosestPoints.scala
@@ -1,6 +1,7 @@
 import java.text.DecimalFormat
 import java.util.Scanner
 
+import scala.annotation.tailrec
 import scala.collection.SortedSet
 import scala.collection.immutable.TreeSet
 
@@ -20,7 +21,37 @@ object ClosestPoints
 
     def core(l: Int, r: Int): Double =
     {
-      val width = r - l
+      @tailrec
+      def find(x: Int, l: Int, r: Int, sameOnRight: Boolean): Int =
+      {
+        if (l >= r) l
+        else
+        {
+//          assert(points(l).x <= x)
+//          assert(x <= points(r).x)
+
+          val m = (l + r) / 2
+          if (if (sameOnRight) x <= points(m).x else x < points(m).x) find(x, l, m, sameOnRight)
+          else find(x, m + 1, r, sameOnRight)
+        }
+      }
+
+      def findRightOf(x: Int, l: Int, r: Int): Int =
+      {
+        val i = find(x, l, r, true)
+
+        if (l < i) assert(points(i - 1).x < x, "findRightOf: " + points(i - 1).x + " not < " + x)
+        i
+      }
+
+      def findLeftOf(x: Int, l: Int, r: Int): Int =
+      {
+        val i = find(x, l, r, false)
+        if (i < r) assert(x < points(i + 1).x, "findLeftOf: " + x + " not < " + points(i + 1).x)
+        i
+      }
+
+      val width = r - l + 1
       assert(width > 1, "width==1 ?")
       if (width == 2) distIdx(l, r)
       else if (width == 3) Seq(distIdx(l, l + 1), distIdx(l + 1, l + 2), distIdx(l, l + 2)).min
@@ -30,18 +61,25 @@ object ClosestPoints
         val ld = core(l, m)
         val rd = core(m + 1, r)
         val d = Math.min(ld, rd)
+        val di = Math.ceil(d).toInt
 
         val mx = points(m).x
 
-        // find in L p.x >= mx - d  -> LD
-        // find points in P in rectangle = RECT
-        // for each LD and RECT calc dist = D2
-        // return min(d, D2.min)
+        val il = findRightOf(mx - di, l, m)
+        val jr = findLeftOf(mx + di, m + 1, r)
+        val ds = for
+        {
+          i <- il to m
+          pl = points(i)
+          if (pl.x >= mx - d)
+          j <- m + 1 to jr
+          pr = points(j)
+          if (pr.x <= mx + d)
+          if (Math.abs(pl.y - pr.y) <= d)
+        } yield dist(pl, pr)
 
-        0.0
-
-
-
+        (d +: ds).min
+        //Math.min(d, ds.min)
       }
 
 
@@ -61,7 +99,9 @@ object ClosestPoints
 
 
     val rawPoints = (for (i <- 0 to n - 1) yield Point(s.nextInt, s.nextInt)) //.toSet(Ordering[Int].on[Point](_.x))
-    val points = SortedSet(rawPoints: _*)(Ordering[Int].on[Point](_.x)).toArray // unique only
+    val uniquePoints = Set(rawPoints: _*) //(Ordering.fromLessThan( (l,r) => if (l.x < r.x) true else l.y < r.y))
+    val points = uniquePoints.toArray.sortBy(_.x)
+    //SortedSet(rawPoints: _*)(Ordering[Int].on[Point](_.x)).toArray // unique only
 
 
     if (points.length < n) println("0.0") // repeated point(s)
--- alg/sbt/src/main/scala/week5/week5.sc
+++ alg/sbt/src/main/scala/week5/week5.sc
@@ -1,7 +1,27 @@
 import scala.collection.SortedSet
+case class Point(x: Int, y: Int)
+Set( Point(0,0), Point(0,0))
+val points = Array(1,2,3,3,7,9,10)
+def find(x: Int, l: Int, r: Int, sameOnRight: Boolean): Int =
+{
+
+  if (l >= r) l
+  else
+  {
+    //assert(points(l) <= x)
+    //assert(x <= points(r))
+
+    val m = (l + r) / 2
+    if (if (sameOnRight) x <= points(m) else x < points(m)) find(x, l, m, sameOnRight)
+    else find(x, m + 1, r, sameOnRight)
+  }
+}
+
+find(5, 0, points.length-1, true)
+find(5, 0, points.length-1, false)
 
 //val a = Array.ofDim[Boolean](2,2)
 //a(1)(1)
-1 until 3
+//1 until 3
 //"1+5".filterNot(_.isDigit)//.map(_.asDigit)
-SortedSet(3, 1, 2, 2).toList
+//SortedSet(3, 1, 2, 2).toArray
