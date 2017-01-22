da209a446b45f99c29f0c632337f96399c8824d3
luke <luke>
2017-01-22 00:05:12 -0500

    week3 - Lottery + ClosestPoints (work in progress)

--- alg/sbt/src/main/scala/ClosestPoints.scala
+++ alg/sbt/src/main/scala/ClosestPoints.scala
@@ -1,15 +1,44 @@
 import java.text.DecimalFormat
 import java.util.Scanner
 
+import scala.collection.SortedSet
+import scala.collection.immutable.TreeSet
 
 
 object ClosestPoints
 {
+
   case class Point(x: Int, y: Int)
+
+  def pow2(x: Double) = x * x
+
+  def dist(l: Point, r: Point) = Math.sqrt(pow2(l.x - r.x) + pow2(l.y - r.y))
 
   def minDistance(points: Array[Point]): Double =
   {
-    0.0
+    def distIdx(l: Int, r: Int) = dist(points(l), points(r))
+
+    def core(l: Int, r: Int): Double =
+    {
+      val width = r - l
+      assert(width > 1, "width==1 ?")
+      if (width == 2) distIdx(l, r)
+      else if (width == 3) Seq(distIdx(l, l + 1), distIdx(l + 1, l + 2), distIdx(l, l + 2)).min
+      else
+      {
+        val m = (l + r) / 2
+        val lr = core(l, m)
+        val rr = core(m + 1, r)
+        val minr = Math.min(lr, rr)
+
+
+      }
+
+
+    }
+
+    if (points.length < 2) 0.0
+    else core(0, points.length - 1)
   }
 
 
@@ -18,14 +47,20 @@ object ClosestPoints
     val s = new Scanner(System.in)
     val formatter = new DecimalFormat("#.####")
 
-
     val n = s.nextInt
 
-    val points = new Array[Point](n)
 
-    for (i <- 0 to n-1) points(i) = Point(s.nextInt, s.nextInt)
+    val rawPoints = (for (i <- 0 to n - 1) yield Point(s.nextInt, s.nextInt)) //.toSet(Ordering[Int].on[Point](_.x))
+    val points = SortedSet(rawPoints: _*)(Ordering[Int].on[Point](_.x)).toArray // unique only
 
-    val min = minDistance(points)
-    println(formatter.format(min))
+
+    if (points.length < n) println("0.0") // repeated point(s)
+    else
+    {
+      val min = minDistance(points)
+      println(formatter.format(min))
+    }
+
+
   }
 }
--- alg/sbt/src/main/scala/week5/week5.sc
+++ alg/sbt/src/main/scala/week5/week5.sc
@@ -1,4 +1,7 @@
+import scala.collection.SortedSet
+
 //val a = Array.ofDim[Boolean](2,2)
 //a(1)(1)
 1 until 3
 //"1+5".filterNot(_.isDigit)//.map(_.asDigit)
+SortedSet(3, 1, 2, 2).toList
