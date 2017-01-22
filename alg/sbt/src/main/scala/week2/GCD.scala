9e93ef669f53a39fb928cd6da8373952b827c4e9
luke <luke>
2017-01-22 18:20:33 -0500

    week1 - resubmitting

--- alg/sbt/src/main/scala/week2/GCD.scala
+++ alg/sbt/src/main/scala/week2/GCD.scala
@@ -1,14 +1,18 @@
 import java.util.Scanner
 
+import scala.annotation.tailrec
+
 object GCD {
   def main(args: Array[String]) = {
     val s = new Scanner(System.in);
 
     val a = s.nextLong();
     val b = s.nextLong();
-    println( lcm(a,b) );
+    println( gcd(a,b) );
+    //println( lcm(a,b) );
   }
 
+  @tailrec
   def gcd(a: Long, b:Long) : Long =
   {
     require(0 <= a, "0 <= a")
@@ -17,7 +21,8 @@ object GCD {
     require(b <= 10e9, "b <= 10e9")
 
     if (a == 1 || b == 1) 1
-    else if (a == 0 || b == 0) 0
+    else if (a == 0) b
+    else if (b == 0) a
     else if (a > b) gcd(b, a % b)
     else gcd(a, b % a)
   }
