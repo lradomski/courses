cc247976f32b65ef852abda777ab208ab123c867
luke <luke>
2017-01-22 19:44:53 -0500

    week2 - FiboSum

--- alg/sbt/src/main/scala/FiboSum.scala
+++ alg/sbt/src/main/scala/FiboSum.scala
@@ -6,42 +6,45 @@ object FiboSum
 {
   def sumLastDigit(n: Long): Int = {
     require(n >= 0, "n >= 0")
-    require(n <= 10*1000*1000, "n <= 10*1000*1000")
+    require(n <= 1e14, "n <= 1e14")
+
 
     val f = new Array[Int](3)
+
+    val max = 60
     var s: Int = 0
+    val ss = new Array[Int](max+1)
 
     def idx(i : Long) : Int = (i % f.length).toInt
 
 
     var i : Long = 0
-    while  (i <= n)
+    f(0) = 0; ss(0) = 0
+    f(1) = 1; ss(1) = 1; s = 1
+    for (i <- 2 to max)
     {
-      if (i <= 1) f( idx(i) ) = i.toInt
-      else {
-        f( idx(i) ) = ( f( idx(i - 1) ) + f( idx(i - 2)) ) % 10
-      }
+      f( idx(i) ) = ( f( idx(i - 1) ) + f( idx(i - 2)) ) % 10
+
 
       s = (s + f(idx(i)))%10
-      i = i + 1;
-    }
 
-    s
+      ss(i) = s
+
+      //print(s + " ")
+    }
+    //println
+
+//    for (i <- 0 to 60)
+//      {
+//        assert(ss(i) == ss(60+i))
+//        assert(ss(60+i) == ss(120+i))
+//      }
+
+    ss( (n % max).toInt )
+    //ss(n.toInt)
   }
 
-//  @tailrec
-//  def sumLastDigit(n: Long): Int =
-//  {
-//    //(sum(n-1) + n%10)%10
-//    if (n == 0) 0
-//    else if (n == 1) 1
-//    else
-//    {
-//      val d = (n % 10).toInt
-//      (d + sumLastDigit(n-1))%10
-//    }
-//
-//  }
+
 
   def main(args: Array[String]): Unit =
   {
@@ -53,3 +56,9 @@ object FiboSum
   }
 
 }
+
+/*
+0 1 3 6 1 9 2 3 7 2 1 5 8 5 5 2 9 3 4 9 5 6 3 1 6 9 7 8 7 7 6 5 3 0 5 7 4 3 9 4 5 1 8 1 1 4 7 3 2 7 1 0 3 5 0 7 9 8 9 9
+0 1 3 6 1 9 2 3 7 2 1 5 8 5 5 2 9 3 4 9 5 6 3 1 6 9 7 8 7 7 6 5 3 0 5 7 4 3 9 4 9
+
+ */
