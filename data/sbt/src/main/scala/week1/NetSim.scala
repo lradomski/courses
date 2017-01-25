c53bd994475e98761ae784f72db634fcce972858
luke <luke>
2017-01-24 20:12:13 -0500

    data / week 1 - NetSim

--- data/sbt/src/main/scala/week1/NetSim.scala
+++ data/sbt/src/main/scala/week1/NetSim.scala
@@ -1,13 +1,85 @@
+import java.io.File
 import java.util.Scanner
 
 import scala.collection.immutable.Queue
+import scala.collection.mutable
 
 object NetSim
 {
 
-  case class Packet(arrival: Int, duration: Int, var processed: Int)
+  case class Packet(arrived: Int, duration: Int, var started: Int)
+
+  def simulate(packets: Array[Packet], bufferSize: Int): Unit =
+  {
+    var in = packets.foldLeft(mutable.Queue[Packet]())((q, p) => {q.enqueue(p); q})
+    var q = mutable.Queue[Packet]()
+    var time = 0
+
+    while (!in.isEmpty || !q.isEmpty)
+    {
+      var times = List[Int]()
+
+      if (!q.isEmpty) // anything to process ?
+      {
+        val p = q.head
+        if (-1 == p.started) p.started = time //start first one if not done yet - first is pending one
+
+        val end = (p.started + p.duration)
+        if (end == time) q.dequeue // time to finish ?
+
+        times = end :: times // time advance point candidate
+      }
+
+      if (!in.isEmpty)
+      {
+        times = in.head.arrived :: times // time advance point candidate
+
+        if (in.head.arrived == time) // did anything arrive ?
+        {
+          val p = in.dequeue
+
+          if (q.size < bufferSize) q.enqueue(p)
+          else assert(p.started == -1)
+        }
+      }
+
+      // advance time - next incoming packet or processing completion point
+      if (!times.isEmpty) time = times.min
+    }
+  }
 
   def main(args: Array[String]): Unit =
+  {
+        for (i <- 1 to 22)
+          {
+            println(">>>>>     " + i)
+            val name = "/Users/luke/git/courses/data/download/Starters PA1/network_packet_processing_simulation/tests/%02d".format(i)
+            val s : Scanner = new Scanner( new File(name) )
+
+            val bufferSize = s.nextInt
+            val n = s.nextInt
+
+            val packets = (for (i <- 1 to n) yield Packet(s.nextInt, s.nextInt, -1)).toArray
+
+            simulate(packets, bufferSize)
+
+            val a = new Scanner( new File(name+".a"))
+            packets.foldLeft(0)(
+              (l,p) =>
+                {
+                  val test = a.nextInt
+                  //assert(p.started == test, l + ": " + p.started + "!=" + test)
+                  val res = (p.started==test);
+                  val line = "%02d".format(i)
+                  if (!res) println(s"${line}: ${res} (${p.started},${test})");
+                  l + 1
+                }
+            )
+          }
+
+  }
+
+  def main2(args: Array[String]): Unit =
   {
     val s = new Scanner(System.in)
 
@@ -15,75 +87,10 @@ object NetSim
     val n = s.nextInt
 
     val packets = (for (i <- 1 to n) yield Packet(s.nextInt, s.nextInt, -1)).toArray
-    //var remaining = packets
 
-    var processing = false
-    var q = Queue[Packet]()
-    var time = 0
+    simulate(packets, bufferSize)
 
-    var i = 0
-
-    while (i < packets.length || !q.isEmpty)
-    {
-      if (q.isEmpty) // nothing to work on
-      {
-        //processing = false
-        if (i < packets.length) // advance time to next packet
-        {
-          assert(time <= packets(i).arrival)
-          time = packets(i).arrival
-        }
-      }
-      else
-      {
-        val pq = q.dequeue
-        val p = pq._1
-        p.processed = time
-        time += p.duration
-        q = pq._2
-      }
-
-      while (i < packets.length && packets(i).arrival <= time)
-      {
-        val p = packets(i)
-
-        if (p.duration == 0 && q.isEmpty) p.processed = time
-        else if (q.size < bufferSize) q = q.enqueue(p)
-        else p.processed = -1
-
-//        if (processing)
-//        {
-//          if (q.size < bufferSize) q = q.enqueue(packets(i))
-//          else packets(i).processed = -1
-//        }
-//        else
-//        {
-//          val p = packets(i)
-//          p.processed = time
-//          time += p.duration
-//          processing = true
-//        }
-
-        i += 1
-      }
-    }
-
-    packets.foreach(p => println(p.processed))
-
-
-    /*
-    3
-
-    0 3 -> 0
-
-    1 1
-    1 1
-    1 1
-    1 1
-
-
-     */
-
+    packets.foreach(p => println(p.started))
   }
 
 }
